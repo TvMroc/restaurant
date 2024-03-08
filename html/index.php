5492eb7e1c1795e44360ecd99a17f2ad712e52c6
TvMroc <1207837@student.roc-nijmegen.nl>
2024-03-08 13:39:59 +0100

    pizza

---
+++ html/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE php>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="main.css">
+        <title>restaurant</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Julee&family=VT323&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="./images/pizza-logo.png">
+            </div>
+            <div class="nav">
+                <a>Home</a>
+                <a>Menu</a>
+                <a>About</a>
+                <div class="spacer"></div>
+                <a>Account</a>
+            </div>
+        </header>
+        <div class="container">
+            <div class="info">
+                <div class="pizza">
+                    <p class="pizza-txt">Discover pizza perfection at our restaurant. where every bite is a delight!</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="shop">
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+                <div class="shop-item"></div>
+            </div>
+        </div>
+    </body>
+</html>
